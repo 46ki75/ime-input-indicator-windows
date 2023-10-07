a4f32c00691d0b7c165de65ee66c0ea826f6004c
shirayuki <46ki75@gmail.com>
2023-10-07 21:14:08 +0900

    Create main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate systray;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = try_main() {
+        println!("Error: {}", e);
+    }
+}
+
+fn try_main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut app = systray::Application::new()?;
+    let _ = app.set_icon_from_file("assets/icon.ico")?;
+    let _ = app.add_menu_item("Say Hello", |_| {
+        println!("Hello, world!");
+        Ok::<_, systray::Error>(())
+    })?;
+    let _ = app.add_menu_item("Exit", |window| {
+        window.quit();
+        Ok::<_, systray::Error>(())
+    })?;
+
+    app.wait_for_message()?;
+    Ok(())
 }
